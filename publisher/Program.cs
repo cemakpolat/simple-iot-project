e19988f1cad734e1621101b63bd75f4fb684119b
Cem Akpolat <akpolat@campus.tu-berlin.de>
2021-12-10 00:47:01 +0100

    publisher and subscriber codes are rewritten in a more proper way

--- publisher/Program.cs
+++ publisher/Program.cs
@@ -12,84 +12,190 @@ using Newtonsoft.Json;
 using Serilog;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace publisher
 {
+     public class Property  {
+        public String objectType { get; set; }
+        
+        // Test the following object type
+        // public String objectType   
+        // {
+        //     get { return String; }   
+        //     set { String = value; }  
+        // }
+
+        public long timestamp { get; set; }
+        public Object value { get; set; }
+        public String key { get; set; }
+        public Property(){}
+        public Property(String otype, long tstamp, String key, Object value){
+            this.objectType = otype;
+            this.timestamp = tstamp;
+            this.key = key;
+            this.value = value;
+        }
+    
+    }
+
+    class IoTEntity {
+
+        
+        public String objectType { get; set; }
+        public String uuid { get; set; }
+        public String groupId { get; set; }
+        public String name { get; set; }
+        public List<Property> properties = new List<Property>();
+        public IoTEntity(){}
+
+        public IoTEntity(String oType, String uuid,  String name, String groupId, List<Property> properties) {
+            this.objectType = oType;
+            this.uuid = uuid;
+            this.groupId = groupId;
+            this.name = name;
+            if(properties != null){
+                this.properties = properties;
+            }
+            
+        }
+        public IoTEntity addProperty(Property item){
+            bool exist = false;
+            foreach(Property element in this.properties){
+                if (element.key == item.key){
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist == false){
+                this.properties.Add(item);
+            }
+            return this;
+        }
+
+    }
+    // TODO: Look at this example https://github.com/chkr1011/MQTTnet/blob/master/Tests/MQTTnet.TestApp.NetCore/PublicBrokerTest.cs
+    class MQTTConnnection{
+        IManagedMqttClient _mqttClient  = null;
+        String client = "Service";
+        String brokerIp = "127.0.0.1";
+        int brokerPort = 1883;
+        ManagedMqttClientOptions options = null;
+        public MQTTConnnection(){
+        
+            this.setup(client, brokerIp, brokerPort);
+            
+        }
+        public void setup(String client, String url, int port){
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                        .WithClientId(client)
+                                        .WithTcpServer(url,port);
+
+            options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+
+            this._mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            this._mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            this._mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            this._mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            this._mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(a => {
+                Log.Logger.Information("Message recieved: {payload}", a.ApplicationMessage);
+                //     var user = JsonConvert.DeserializeObject<Object>( a.ApplicationMessage);
+            });
+            
+
+            _mqttClient.UseApplicationMessageReceivedHandler(e => handleMessage(e.ApplicationMessage)
+            // {
+            //     Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+            //     Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
+            //     Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+            //     Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
+            //     Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
+            //     Console.WriteLine();
+            //     store.insert(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+
+                
+            // }
+            );
+
+            _mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            // _mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopic("dev.to/topic/json").Build());
+
+           
+        }
+        private void handleMessage(MqttApplicationMessage message){
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                Console.WriteLine($"+ Topic = {message.Topic}");
+                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(message.Payload)}");
+                Console.WriteLine($"+ QoS = {message.QualityOfServiceLevel}");
+                Console.WriteLine($"+ Retain = {message.Retain}");
+                Console.WriteLine();
+
+                //store.insert(Encoding.UTF8.GetString(message.Payload));
+        }
+
+        public void subscribe(String topic){
+            this._mqttClient.SubscribeAsync(
+             new MqttTopicFilter
+                 {
+                    Topic = topic //"dev.to/topic/json"
+                }
+            );
+
+        }
+        
+         public void sendMessage(String topic, String json){
+             Task.Run(() => this._mqttClient.PublishAsync(topic, json));
+            this._mqttClient.PublishAsync(topic, json);
+        }
+        
+        public void OnConnected(MqttClientConnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully connected.");
+        }
+
+        public  void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        {
+            Log.Logger.Warning("Couldn't connect to broker.");    
+        }
+        
+
+        public void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully disconnected.");
+            
+        }
+    }
+
+    
     class Program
     {
 
          static void Main(string[] args)
         {
-            DataStore store = new DataStore();
+            
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
                 .CreateLogger();
 
-            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
-                                        .WithClientId("Server")
-                                        .WithTcpServer("127.0.0.1", 1883);
 
-            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
-                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
-                                    .WithClientOptions(builder.Build())
-                                    .Build();
-
-            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
-
-            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
-            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
-            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
-
-            // _mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(a => {
-            //     Log.Logger.Information("Message recieved: {payload}", a.ApplicationMessage);
-            //     var user = JsonConvert.DeserializeObject<Object>( a.ApplicationMessage);
-            //     Console.WriteLine(user);
-
-            // });
-            _mqttClient.UseApplicationMessageReceivedHandler(e =>
-            {
-                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
-                Console.WriteLine($"+ Topic = {e.ApplicationMessage.Topic}");
-                Console.WriteLine($"+ Payload = {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
-                Console.WriteLine($"+ QoS = {e.ApplicationMessage.QualityOfServiceLevel}");
-                Console.WriteLine($"+ Retain = {e.ApplicationMessage.Retain}");
-                Console.WriteLine();
-                store.insert(Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
-
-                // Task.Run(() => mqttClient.PublishAsync("hello/world"));
-            });
-
-            _mqttClient.StartAsync(options).GetAwaiter().GetResult();
-
-            // _mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopic("dev.to/topic/json").Build());
-
-           _mqttClient.SubscribeAsync(
-             new MqttTopicFilter
-                 {
-                    Topic = "dev.to/topic/json"
-                }
-            );
+            MQTTConnnection mqtt = new MQTTConnnection();
+            
+            DataStore store = new DataStore();
+           
             Console.ReadLine();
 
         }
-
-        public static void OnConnected(MqttClientConnectedEventArgs obj)
-        {
-            Log.Logger.Information("Successfully connected.");
-        }
-
-        public static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
-        {
-            Log.Logger.Warning("Couldn't connect to broker.");
-        }
-
-        public static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
-        {
-            Log.Logger.Information("Successfully disconnected.");
-        }
     }
+
     class DataStore{
 
         public DataStore(){}
@@ -102,7 +208,7 @@ namespace publisher
 
             var doc = new BsonDocument
             {
-                {"measuremetn", measurement}
+                {"measurement", measurement}
             };
 
             cars.InsertOne(doc);
--- subscriber/Program.cs
+++ subscriber/Program.cs
@@ -8,40 +8,100 @@ using MQTTnet.Client.Receiving;
 using MQTTnet.Extensions.ManagedClient;
 using Newtonsoft.Json;
 using Serilog;
+using System.Collections;
+using System.Collections.Generic;
+
+
 namespace subscriber
-{
-    class Program
-    {
+{   
+      public class Property  {
+        public String objectType { get; set; }
 
-         static void Main(string[] args)
-        {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .CreateLogger();
+        public long timestamp { get; set; }
+        public Object value { get; set; }
+        public String key { get; set; }
+        public Property(){}
+        public Property(String otype, long tstamp, String key, Object value){
+            this.objectType = otype;
+            this.timestamp = tstamp;
+            this.key = key;
+            this.value = value;
+        }
+    
+    }
 
+    class IoTEntity {
+
+        
+        public String objectType { get; set; }
+        public String uuid { get; set; }
+        public String groupId { get; set; }
+        public String name { get; set; }
+        public List<Property> properties = new List<Property>();
+        public IoTEntity(){}
+
+        public IoTEntity(String oType, String uuid,  String name, String groupId, List<Property> properties) {
+            this.objectType = oType;
+            this.uuid = uuid;
+            this.groupId = groupId;
+            this.name = name;
+            if(properties != null){
+                this.properties = properties;
+            }
+            
+        }
+        public IoTEntity addProperty(Property item){
+            bool exist = false;
+            foreach(Property element in this.properties){
+                if (element.key == item.key){
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist == false){
+                this.properties.Add(item);
+            }
+            return this;
+        }
+
+    }
+
+    class MQTTConnnection{
+        IManagedMqttClient _mqttClient  = null;
+        String client = "Sensor";
+        String brokerIp = "127.0.0.1";
+        int brokerPort = 1883;
+        ManagedMqttClientOptions options = null;
+        public MQTTConnnection(){
+        
+            this.setup(client, brokerIp, brokerPort);
+            
+        }
+        public void setup(String client, String url, int port){
             MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
-                                        .WithClientId("Cem")
-                                        .WithTcpServer("127.0.0.1", 1883);
+                                        .WithClientId(client)
+                                        .WithTcpServer(url,port);
 
-            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+            options = new ManagedMqttClientOptionsBuilder()
                                     .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
                                     .WithClientOptions(builder.Build())
                                     .Build();
 
-            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+            this._mqttClient = new MqttFactory().CreateManagedMqttClient();
 
-            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
-            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
-            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+            this._mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            this._mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            this._mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
 
-            _mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(a => {
+            this._mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(a => {
                 Log.Logger.Information("Message recieved: {payload}", a.ApplicationMessage);
             });
+            //                await client.ConnectAsync(options);
 
             _mqttClient.StartAsync(options).GetAwaiter().GetResult();
-
+        }
+        
+        public void startLoop(){
             while (true)
             {
                 string json = JsonConvert.SerializeObject(new { message = "Heyo :)", sent= DateTimeOffset.UtcNow });
@@ -49,22 +109,75 @@ namespace subscriber
 
                 Task.Delay(4000).GetAwaiter().GetResult();
             }
-        }
 
-        public static void OnConnected(MqttClientConnectedEventArgs obj)
+        }
+         public void sendMessage(String topic, String json){
+            this._mqttClient.PublishAsync(topic, json);
+        }
+        
+        public void OnConnected(MqttClientConnectedEventArgs obj)
         {
             Log.Logger.Information("Successfully connected.");
         }
 
-        public static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        public  void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
         {
-            Log.Logger.Warning("Couldn't connect to broker.");
+            Log.Logger.Warning("Couldn't connect to broker.");    
         }
+        
 
-        public static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        public void OnDisconnected(MqttClientDisconnectedEventArgs obj)
         {
             Log.Logger.Information("Successfully disconnected.");
+            
         }
     }
+
     
+   
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            // create the object and send the first message
+
+            IoTEntity pe = new IoTEntity("sensor","uuid", "assembly", "department", null);
+            pe.addProperty(new Property("type",111,"temperature",0));
+            // serialize
+            var jsonnew = JsonConvert.SerializeObject(pe);
+
+            Console.WriteLine(jsonnew);
+            // deserialize, this is required on the subscriber side
+            IoTEntity ped = JsonConvert.DeserializeObject<IoTEntity>(jsonnew);
+
+            Console.WriteLine(ped.name);
+         
+            MQTTConnnection mqtt = new MQTTConnnection();
+            
+            measure(mqtt, pe);
+            
+
+        } 
+        static void measure(MQTTConnnection mqtt, IoTEntity entity){
+            
+            while (true)
+            {
+                // do measurement 
+                
+                string json = JsonConvert.SerializeObject(new { message = "Heyo :)", sent= DateTimeOffset.UtcNow });
+
+                mqtt.sendMessage("dev.to/topic/json", json);
+
+                Task.Delay(4000).GetAwaiter().GetResult();
+            }
+
+        }
+}
 }
